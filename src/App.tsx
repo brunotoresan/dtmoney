7833509dd7d0bc4efa936e8a681384751ab113f6
Bruno Ramos Toresan <brunotoresan@gmail.com>
2021-06-16 20:11:19 -0300

    adding context

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { GlobalStyle } from "./styles/global";
 import { Header } from "./components/Header"
 import { Dashboard } from "./components/Dashboard";
 import { NewTransactionModal } from "./components/NewTransactionModal"
+import { TransactionsContext } from './TransactionsContext';
 
 // https://reactcommunity.org/react-modal/accessibility/
 Modal.setAppElement('#root')
@@ -21,7 +22,7 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsContext.Provider value={[]}>
       <GlobalStyle />
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
@@ -29,6 +30,6 @@ export function App() {
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
       />
-    </>
+    </TransactionsContext.Provider>
   );
 }
---
+++ src/TransactionsContext.ts
@@ -0,0 +1,4 @@
+import { createContext } from "react"
+
+// The provider property will make the context available for other components 
+export const TransactionsContext = createContext([])
